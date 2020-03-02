e6a189a54fcb51192051ece32d35607e766c1c79
Cavoi Rom <cavoirom.wanted@yahoo.com>
2020-03-02 10:35:22 +0700

    Create nodejs project

---
+++ build/public/asset/site/index.js
@@ -0,0 +1,8 @@
+(function(global) {
+  'use strict';
+  // project-nxv/src/public/index.js
+  var ___R$project$nxv$src$public$index_js = {};
+
+
+  return ___R$project$nxv$src$public$index_js;
+})(typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : this);
