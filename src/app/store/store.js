ba850eaad7fd81b4993ae98f6315c9bf8081a740
Cavoi Rom <cavoirom.wanted@yahoo.com>
2020-05-17 15:35:45 +0700

    Configured Redux Devtools.

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -2,10 +2,22 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducer';
 import { createEpicMiddleware } from 'redux-observable';
 import rootEpic from './epic';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore() {
+  const initialState = {
+    site: {},
+    me: {},
+    journey: {
+      years: [],
+    },
+    work: {
+      years: [],
+    },
+  };
+
   const epicMiddleware = createEpicMiddleware();
-  const store = createStore(rootReducer, applyMiddleware(epicMiddleware));
+  const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(epicMiddleware)));
   epicMiddleware.run(rootEpic);
   return store;
 }
