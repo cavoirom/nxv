6d61667c3a557270c17c04290f432f312ff62cc8
Cavoi Rom <cavoirom.wanted@yahoo.com>
2020-05-26 09:01:28 +0700

    Added script to generate cache routes.

---
+++ scripts/generate-cache-route.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+const directoryPath = path.join(__dirname, '../dist');
+const excludedPaths = ['api', 'worker.js', 'worker.js.map', 'CNAME'];
+const additionalPaths = [''];
+
+fs.readdir(directoryPath, (err, fileNames) => {
+  if (err) {
+    return console.log(`Unable to scan directory: ${err}`);
+  }
+
+  const cacheRoutes = fileNames
+    .concat(additionalPaths)
+    .filter((fileName) => !excludedPaths.includes(fileName))
+    .map((fileName) => `'/${fileName}'`)
+    .join(', ');
+  console.log(cacheRoutes);
+});
