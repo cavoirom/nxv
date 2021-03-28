66f451b1f3d8b88ffec658a532e3d4c4567b7af9
Cavoi Rom <cavoirom.wanted@yahoo.com>
2021-03-29 00:21:23 +0700

    Added nxv-router source.

---
+++ modules/router/index.js
@@ -0,0 +1,43 @@
+const REDIRECT_ROUTES = new Map();
+REDIRECT_ROUTES.set("/blog/index.html", "/blog");
+REDIRECT_ROUTES.set("/blog/", "/blog");
+REDIRECT_ROUTES.set("/home/index.html", "/home");
+REDIRECT_ROUTES.set("/home/", "/home");
+// DEFAULT ROUTE IS /blog
+REDIRECT_ROUTES.set("/index.html", "/blog");
+REDIRECT_ROUTES.set("/", "/blog");
+
+// Should redirect nguyenxuanvinh.com/blog/entry/*/index.html
+const REDIRECT_ENTRY_PATTERN = /(?:(\/blog\/entry\/[\d\/]+[\w-]+)(\/(?:index\.html)?))$/;
+
+const REWRITE_ROUTES = new Map();
+REWRITE_ROUTES.set("/blog", "/blog/index.html");
+REWRITE_ROUTES.set("/home", "/home/index.html");
+
+// Should rewrite nguyenxuanvinh.com/blog/entry/<blog-post-title>
+const REWRITE_ENTRY_PATTERN = /(?:\/blog\/entry\/[\d\/]+[\w-]+)$/;
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+
+  // Try to do simple url mapping without RegEx for common urls
+  if (REDIRECT_ROUTES.has(url.pathname)) {
+    return Response.redirect(
+      url.origin + REDIRECT_ROUTES.get(url.pathname),
+      301
+    );
+  } else if (REWRITE_ROUTES.has(url.pathname)) {
+    return fetch(url.origin + REWRITE_ROUTES.get(url.pathname));
+  } else if (REDIRECT_ENTRY_PATTERN.test(url.pathname)) {
+    const result = REDIRECT_ENTRY_PATTERN.exec(url.pathname);
+    return Response.redirect(result[1], 301);
+  } else if (REWRITE_ENTRY_PATTERN.test(url.pathname)) {
+    return fetch(url.href + "/index.html");
+  }
+
+  return fetch(request);
+}
