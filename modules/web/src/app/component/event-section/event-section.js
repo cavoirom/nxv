38e37d38979fac5fd4b966e5a3602f54195b0c96
Cavoi Rom <cavoirom.wanted@yahoo.com>
2021-06-12 09:39:12 +0700

    Added tests for event-section.

--- modules/web/src/app/component/event-section/event-section.js
+++ modules/web/src/app/component/event-section/event-section.js
@@ -1,11 +1,11 @@
 import { h } from 'preact';
 
 function renderEvent(event, index) {
-  return h('li', { key: index }, event);
+  return h('li', { key: index, 'aria-label': `event ${event}` }, event);
 }
 
 export default function EventSection({ year }) {
-  const yearTitle = h('h4', null, year.year);
-  const events = h('ul', null, year.events.map(renderEvent));
+  const yearTitle = h('h4', { 'aria-label': `year ${year.year}` }, year.year);
+  const events = h('ul', { 'aria-label': `events in ${year.year}` }, year.events.map(renderEvent));
   return h('div', null, yearTitle, events);
 }
--- modules/web/test/app/component/event-section/event-section.test.js
+++ modules/web/test/app/component/event-section/event-section.test.js
@@ -6,20 +6,18 @@ import { h } from 'preact';
 import { render, screen } from '@testing-library/preact';
 import EventSection from '../../../../src/app/component/event-section/event-section';
 
-test('event-section rendering', async () => {
+test('event-section should have correct roles and accessible name', async () => {
   const year = {
     year: 2020,
     events: ['started this site.'],
   };
   render(h(EventSection, { year }));
 
-  // Assert year
-  const yearField = await screen.findByText(year.year);
-  expect(yearField.tagName).toEqual('H4');
-  expect(yearField.textContent).toEqual(year.year.toString());
+  // Assert events
+  const yearTitle = await screen.findByRole('heading', { name: `year ${year.year}` });
+  expect(yearTitle.textContent).toEqual(year.year.toString());
 
   // Assert events
-  const firstEvent = await screen.findByText(year.events[0]);
-  expect(firstEvent.tagName).toEqual('LI');
+  const firstEvent = await screen.findByRole('listitem', { name: `event ${year.events[0]}` });
   expect(firstEvent.textContent).toEqual(year.events[0]);
 });
