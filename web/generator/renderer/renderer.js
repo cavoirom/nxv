efe246903731463b4e10290df0e81147f42ee99c
Cavoi Rom <cavoirom.wanted@yahoo.com>
2024-10-25 18:35:16 +0700

    Removed unnecessary escapes.

--- web/generator/renderer/renderer.js
+++ web/generator/renderer/renderer.js
@@ -26,7 +26,7 @@ export default class Renderer {
     );
     const index = this.indexTemplate
         .replace(`<title>vinh</title>`, `<title>${page.state.site.title}</title>`)
-        .replace(`<div id="app"></div>`, `<div id=\"app\">${appHtml}</div>`)
+        .replace(`<div id="app"></div>`, `<div id="app">${appHtml}</div>`)
         .replace(`window.__STATE__ = undefined;`, `window.__STATE__ = fetch('${page.url}/index.json').then(response => response.json());`);
     return index;
   }
